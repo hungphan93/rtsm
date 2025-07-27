3786f54b133aecf627186cd9dd29b32f55455503
hungphan <hungvanphan93@gmail.com>
2025-07-27 20:47:22 +0700

    hint2

--- build_install_dependency.sh
+++ build_install_dependency.sh
@@ -68,10 +68,11 @@ export QML2_IMPORT_PATH=$QT_PREFIX/qml
 
 # === Environment Info ===
 echo "===> Using Qt from: $QT_PREFIX"
+echo "===> Home: $PWD"
 
 # === Download and Extract Qt ===
 echo "===> Downloading Qt.tar.xz..."
-wget -q --show-progress "https://drive.usercontent.google.com/download?id=1V7t8o21LFvt2BctjjoaOiqQZREqV5ExY&export=download&confirm=t&uuid=b4a6b4bb-ff0b-44f5-acce-4fcfd37c72ad" -O Qt.tar.xz
+wget -q "https://drive.usercontent.google.com/download?id=1V7t8o21LFvt2BctjjoaOiqQZREqV5ExY&export=download&confirm=t&uuid=b4a6b4bb-ff0b-44f5-acce-4fcfd37c72ad" -O Qt.tar.xz
 
 echo "===> Extracting Qt.tar.xz..."
 mkdir -p "$USER_HOME/Qt"
